64d64e3233deb2ea07ec315c9f9b215887ea01a4
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-02-26 19:27:33 +0000

    adding package

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/*
+function createOuterError(err, msg) {
+    const e = new Error();
+    e.stack = e.stack.split('\n').slice(3).filter(line => line.match(/\(.*(\\+|\/+).*\)/)).join('\n');
+    e.message = msg;
+    throw e;
+}
+*/
+
+module.exports = (options, def) => {
+    def = def || {};
+    if (options) {
+        for (const a in options) {
+        }
+    } else {
+        options = {};
+    }
+    // set defaults here:
+    for (const a in def) {
+
+    }
+};
