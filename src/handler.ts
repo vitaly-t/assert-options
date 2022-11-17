9374b29ee5d680b5a19b5edf4019782543228e36
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-17 04:23:44 +0000

    initial implementation

--- src/handler.ts
+++ src/handler.ts
@@ -10,17 +10,22 @@ export interface IErrorContext {
     key?: string;
 }
 
+export type assertType = (options: NamedValues | null | undefined, defaults: NamedValues | string[]) => NamedValues;
+
+export type NamedValues = { [name: string]: any };
+
 export interface IOptionsErrorHandler {
-    handle(err: OptionsError, ctx: IErrorContext): void;
+    handle(err: OptionsError, ctx: IErrorContext): NamedValues;
 }
 
 export class EmptyErrorHandler implements IOptionsErrorHandler {
-    handle(err: OptionsError, ctx: IErrorContext): void {
+    handle(err: OptionsError, ctx: IErrorContext): NamedValues {
+        return ctx.options;
     }
 }
 
 export class DefaultErrorHandler implements IOptionsErrorHandler {
-    handle(err: OptionsError, ctx: IErrorContext): void {
+    handle(err: OptionsError, ctx: IErrorContext): NamedValues {
         switch (err) {
             case OptionsError.invalidOptionsParam:
                 throw new TypeError(`Invalid "options" parameter: ${JSON.stringify(ctx.options)}`);
@@ -31,5 +36,6 @@ export class DefaultErrorHandler implements IOptionsErrorHandler {
             default:
                 break;
         }
+        return ctx.options; // this should never happen;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,34 @@
-export type NamedValues = { [name: string]: any };
+import {assertType, DefaultErrorHandler, IOptionsErrorHandler, NamedValues, OptionsError} from './handler';
 
-export function assertOptions(options: NamedValues | null | undefined, defaults: NamedValues | string[]): NamedValues {
-    if (options !== null && options !== undefined && typeof options !== 'object') {
-        throw new TypeError('Invalid "options" parameter: ' + JSON.stringify(options));
-    }
-    const isArray = Array.isArray(defaults);
-    if (!isArray && (!defaults || typeof defaults !== 'object')) {
-        throw new TypeError('Invalid "defaults" parameter: ' + JSON.stringify(defaults));
-    }
-    if (options) {
-        for (const a of Object.keys(options)) {
-            if ((isArray && defaults.indexOf(a) === -1) || (!isArray && !(a in defaults))) {
-                throw new Error('Option "' + a + '" is not recognized.');
+export function configAssert(errHandler: IOptionsErrorHandler): assertType {
+
+    return function (options: NamedValues | null | undefined, defaults: NamedValues | string[]): NamedValues {
+        if (options !== null && options !== undefined && typeof options !== 'object') {
+            return errHandler.handle(OptionsError.invalidOptionsParam, {options, defaults});
+        }
+        const isArray = Array.isArray(defaults);
+        if (!isArray && (!defaults || typeof defaults !== 'object')) {
+            return errHandler.handle(OptionsError.invalidDefaultsParam, {options, defaults});
+        }
+        if (options) {
+            for (const key of Object.keys(options)) {
+                if ((isArray && defaults.indexOf(key) === -1) || (!isArray && !(key in defaults))) {
+                    return errHandler.handle(OptionsError.optionNotRecognized, {options, defaults, key});
+                }
+            }
+        } else {
+            options = {};
+        }
+        if (!isArray) {
+            const defs = <NamedValues>defaults;
+            for (const d of Object.keys(defs)) {
+                if (options[d] === undefined && defs[d] !== undefined) {
+                    options[d] = defs[d];
+                }
             }
         }
-    } else {
-        options = {};
-    }
-    if (!isArray) {
-        const defs = <NamedValues>defaults;
-        for (const d of Object.keys(defs)) {
-            if (options[d] === undefined && defs[d] !== undefined) {
-                options[d] = defs[d];
-            }
-        }
-    }
-    return options;
+        return options;
+    };
 }
+
+export const assertOptions: assertType = configAssert(new DefaultErrorHandler());
